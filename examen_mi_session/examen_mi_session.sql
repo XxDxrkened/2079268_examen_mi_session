c680424e6d8559ed0510758dfd505d4f5d337510
XxDxrkened <nikoslegeremelanson@gmail.com>
2024-02-26 15:14:04 -0400

    Update examen_mi_session.sql

--- examen_mi_session/examen_mi_session.sql
+++ examen_mi_session/examen_mi_session.sql
@@ -1,5 +1,8 @@
 USE epharmacy;
 
 SELECT user.full_name AS 'Nom Complet', role.name AS Role 
-FROM user LEFT JOIN role ON user.role_id = role.id
+FROM user LEFT JOIN role ON user.role_id = role.id;
 
+SELECT product.name AS Designation, COUNT(quantity) AS Quantite
+FROM OrderLine JOIN product ON OrderLine.id = product.id
+GROUP BY product.name;
